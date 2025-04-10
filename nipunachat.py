4099b9e8d4cc620ac96f61b39337fab890281799
JawanWeb <jspandana4rdotnet@gmail.com>
2025-04-10 14:55:14 +0530

    Add files via upload

--- nipunachat.py
+++ nipunachat.py
@@ -6,12 +6,12 @@ import tempfile
 
 # Azure OpenAI Client
 client = AzureOpenAI(
-    api_key="3SpmpodXBzKYWZnIBcAkxDR5DTFJfDBRFWlASKb1sY7Fe52e2CTbJQQJ99BDACYeBjFXJ3w3AAABACOGlumZ",
+    api_key="CMfEAxVMt34Nq8hQlkz9erOq7PcvKhNCvtyCXB7PhT8ypv7vctuyJQQJ99BDACHYHv6XJ3w3AAABACOGGQbS",
     api_version="2024-12-01-preview",
-    azure_endpoint="https://pavani-day3.openai.azure.com/"
+    azure_endpoint="https://cybersofttrainingday2.openai.azure.com"
 )
 
-st.title("🧠 Multi Modal Chatbot")
+st.title("🧠  Multi Modal Chatbot")
 
 # Session State Initialization
 if "messages" not in st.session_state:
@@ -23,35 +23,22 @@ if "messages" not in st.session_state:
 if "file_uploaded" not in st.session_state:
     st.session_state.file_uploaded = False
 
-# File Uploader (PDF and Excel only)
+# File Uploader (Excel only)
 uploaded_file = st.file_uploader(
-    "Upload PDF or Excel file",
-    type=["pdf", "xlsx", "jpg", "png", "jpeg"],
-    accept_multiple_files =True
+    "Upload  Excel file",
+    type=["xlsx"],
+    accept_multiple_files=True
 )
 
-file_text = []
+file_text = ""
 
-
-for file in uploaded_file:
-    file_type = file.type
-    uploaded_file =file 
-
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-        tmp_file.write(uploaded_file.read())
-        tmp_path = tmp_file.name
-
-    if "pdf" in file_type:
-        with pdfplumber.open(tmp_path) as pdf:
-            file_text = "\n".join([page.extract_text() for page in pdf.pages if page.extract_text()])
-
-    elif "excel" in file_type or "spreadsheet" in file_type:
-        df = pd.read_excel(tmp_path)
-        file_text = df.to_markdown()
-    elif "image" in file_type:
-        st.image(tmp_path, caption="Uploaded Image", use_column_width=True)
-        file_text = "Image uploaded. Please describe the content or ask a question about it."
-
+if uploaded_file and not st.session_state.file_uploaded:
+    for uploaded_file in uploaded_file:
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(uploaded_file.read())
+            temp_path = temp_file.name
+            df = pd.read_excel(temp_path)
+            file_text += df.to_markdown()+"\n"
     if file_text:
         st.session_state.messages.append({
             "role": "user",
